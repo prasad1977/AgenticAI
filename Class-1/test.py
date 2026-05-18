92b05642c575dc9ce17abb8660cc21e92c2fe611
Prasad Bellam <30711544+prasad-ai-ux@users.noreply.github.com>
2026-05-18 11:53:36 -0500

    Update test.py

--- Class-1/test.py
+++ Class-1/test.py
@@ -6,10 +6,7 @@ from langchain_core.messages import HumanMessage
 
 print("Testing Basic LangChain Setup...\n")
 # Initialize LLM
-#os.environ["OPENAI_API_KEY"]="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" # Replace with your actual key
-
-#Naresh Key
-os.environ["OPENAI_API_KEY"]="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"
+os.environ["OPENAI_API_KEY"]=""
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 # Test 1: Simple query
